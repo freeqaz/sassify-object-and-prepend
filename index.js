08c7df318984572babfaaf7ba9e90225ae251c66
Johnathan Free Wortley <jwortley@uber.com>
2015-05-23 13:16:30 -0700

    Initial working version!

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-module.exports = function sassVariablesUtilFn() {
+'use strict';
+var fs = require('fs');
+var sassVariables = require('sass-variables');
 
-};
+module.exports = sassVariablesUtilFn;
 
+/**
+ * Takes in a javascript object and converts it to SASS variables.
+ * The variables are prepended to the top of the file at the inputPath
+ * And written to the path of the outputFile.
+ * Runs IO operations synchronously.
+ * @param {object} variables Javascript object to Sassify.
+ * @param {string} inputPath Path to file to read.
+ * @param {string} outputPath Path to file to write.
+ * @returns {string} Output that was written to file.
+ */
+function sassVariablesUtilFn(variables, inputPath, outputPath) {
+  var inputFile = fs.readFileSync(inputPath);
+
+  var outputFile = sassVariables(variables, inputFile);
+
+  fs.writeFileSync(outputPath, outputFile);
+
+  return outputFile;
+}
--- test/index.js
+++ test/index.js
@@ -1,8 +1,24 @@
+'use strict';
+var fs = require('fs');
 var test = require('tape');
-var sassVariables = require('../');
+var sassVariablesUtil = require('../');
 
-test('that module exports correctly', function t(assert) {
-  assert.ok(sassVariables, 'exported correctly');
+test('Appends variables and writes as expected', function t(assert) {
+  var outputFile = sassVariablesUtil({
+    blue: '#0000FF'
+  }, './test/test.scss', './test/test-output.scss');
+
+  var split = outputFile.split('\n');
+
+  assert.equal(split[0], '$blue: "#0000FF";', 'blue is blue');
+  assert.equal(
+    split[1],
+    '.whatever { color: $blue; }',
+    'extra content is written'
+  );
+
+  var outputStat = fs.statSync('./test/test-output.scss');
+  assert.ok(outputStat, 'output file was written correctly');
+
   assert.end();
 });
-
